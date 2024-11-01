3a70a5a1ddbdff5ab0d95fdc44f5384f703e99f4
unknown <pedrohenriquefdasilva14@gmail.com>
2024-11-01 19:45:14 -0300

    feat: create userEntity, entity class

---
+++ src/main/java/tech/silva/online_academy/Entity/Role.java
@@ -0,0 +1,6 @@
+package tech.silva.online_academy.Entity;
+
+
+public enum Role {
+    ROLE_STUDENT, ROLE_TEACHER
+}
---
+++ src/main/java/tech/silva/online_academy/Entity/UserEntity.java
@@ -0,0 +1,24 @@
+package tech.silva.online_academy.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor @NoArgsConstructor @Getter @Setter
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String username;
+    private String password;
+    private Long registrationNumber;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+}
