0ccbc1c72a2b7dda417bbf099f223a219191a8e7
unknown <pedrohenriquefdasilva14@gmail.com>
2024-11-01 19:36:08 -0300

    feat: create project

---
+++ src/test/java/tech/silva/online_academy/OnlineAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.silva.online_academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
